e77e7f90e47451e0b6cfda6337353ece6f3fbdf1
zetathealmighty <ti.gouveia21@gmail.com>
2023-12-29 14:11:59 +0000

    standardized function io

--- main.hs
+++ main.hs
@@ -19,7 +19,7 @@ data Inst =
 type Code = [Inst]
 
 
--- run :: (Code, Stack, State) -> (Code, Stack, State)
+run :: (Code, Stack, State) -> (Code, Stack, State)
 run = undefined -- TODO
 
 -- To help you test your assembler
@@ -27,43 +27,55 @@ testAssembler :: Code -> (String, String)
 testAssembler code = (stack2Str stack, state2Str state)
   where (_,stack,state) = run (code, createEmptyStack, createEmptyState)
 
-add :: Stack -> Stack 
-add s = Stk.push ((Elm.+) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s))
+add :: (Code, Stack, State) -> (Code, Stack, State) 
+add (code, stack, state) = (code, Stk.push ((Elm.+) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s)), state)
 
-sub :: Stack -> Stack 
-sub s = Stk.push ((Elm.-) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s))
+sub :: (Code, Stack, State) -> (Code, Stack, State) 
+sub (code, stack, state) = (code, Stk.push ((Elm.-) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s)), state)
 
-mul :: Stack -> Stack 
-mul s = Stk.push ((Elm.*) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s))
+mul :: (Code, Stack, State) -> (Code, Stack, State) 
+mul (code, stack, state) = (code, Stk.push ((Elm.*) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s)), state)
 
-eq :: Stack -> Stack
-eq s = Stk.push ((Elm.==) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s))
+eq :: (Code, Stack, State) -> (Code, Stack, State)
+eq (code, stack, state) = (code, Stk.push ((Elm.==) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s)), state)
 
-le :: Stack -> Stack
-le s = Stk.push ((Elm.<=) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s))
+le :: (Code, Stack, State) -> (Code, Stack, State)
+le (code, stack, state) = (code, Stk.push ((Elm.<=) (Stk.top s) (Stk.top (Stk.pop s))) (Stk.pop (Stk.pop s)), state)
 
-tru :: Stack -> Stack
-tru s = Stk.push (Elm.B True) s
+tru :: (Code, Stack, State) -> (Code, Stack, State)
+tru (code, stack, state) = (code, Stk.push (Elm.B True) s, state)
 
-fals :: Stack -> Stack 
-fals s = Stk.push (Elm.B False) s
+fals :: (Code, Stack, State) -> (Code, Stack, State) 
+fals (code, stack, state) = (code, Stk.push (Elm.B False) s, state)
 
-push :: Stack -> Integer -> Stack
-push s v = Stk.push (Elm.I v) s
+push :: (Code, Stack, State) -> Integer -> (Code, Stack, State)
+push (code, stack, state) v = (code, Stk.push (Elm.I v) s, state)
 
-neg :: Stack -> Stack
-neg s = let n1 = Stk.top s in let s2 = Stk.pop s in Stk.push (Elm.not n1) s2
+neg :: (Code, Stack, State) -> (Code, Stack, State)
+neg (code, stack, state) = let n1 = Stk.top s in 
+  let s2 = Stk.pop s in 
+  Stk.push (Elm.not n1) s2
 
-fetch :: Stack -> String -> State -> Stack
-fetch stack str state = let n1 = Stk.top stack in let newVal = Stt.find str state in case newVal of 
- Just a -> Stk.push a stack
- Nothing -> error "Runtime error"
+fetch :: (Code, Stack, State) -> String -> (Code, Stack, State)
+fetch (code, stack, state) str = let n1 = Stk.top stack in 
+  let newVal = Stt.find str state in 
+  case newVal of 
+    Just a -> (code, Stk.push a stack, state)
+    Nothing -> error "Runtime error"
 
-store :: Stack -> String -> State -> (State,Stack)
-store stack str state = let newVal = Stk.top stack in let newState = Stt.push str newVal state in (newState,Stk.pop stack)
+store :: (Code, Stack, State) -> String -> (Code, Stack, State)
+store (code, stack, state) str = let newVal = Stk.top stack in 
+  let newState = Stt.push str newVal state in 
+  (code, Stk.pop stack, newState)
 
-noop :: Stack -> State -> (Stack, State)
-noop stack state = (stack,state)
+branch :: (Code, Stack, State) -> Code -> Code -> (Code, Stack, State)
+branch (code, stack, state) yes no = case (Stk.top stack) of 
+  Elm.B True -> (yes ++ code, Stk.pop stack, state)
+  Elm.B False -> (no ++ code, Stk.pop stack, state)
+  _ -> error "Runtime error"
+
+noop :: (Code, Stack, State) -> (Code, Stack, State)
+noop (code, stack, state) = (code, stack, state)
 
 -- Examples:
 -- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
@@ -85,168 +97,4 @@ noop stack state = (stack,state)
 -- Part 2
 
 -- TODO: Define the types Aexp, Bexp, Stm and Program
-
-data ALit = IntValue Integer | IntVariable String
-
-data Aexp
-  = IntLit ALit
-  | IntAdd    Aexp Aexp
-  | IntMult   Aexp Aexp
-  | IntSub    Aexp Aexp
-
-data BLit = BoolValue Bool | BoolVariable String
-
-data Bexp
-  = BoolLit  BLit
-  | BoolNeg    Bexp
-  | BoolAnd    Bexp Bexp
-  | BoolEqual  Bexp Bexp
-  | IntEqual  Aexp Aexp
-  | IntLe     Aexp Aexp
-
-data Stm
-  = IfStm Bexp [Stm] [Stm]
-  | LoopStm Bexp [Stm]
-  | AssignIntStm String Aexp
-  | AssignBoolStm String Bexp
-
-type Program = [Stm]
-
-compA :: Aexp -> Code
-compA (IntLit (IntValue n)) = [Push n]
-compA (IntLit (IntVariable var)) = [Fetch var]
-compA (IntAdd exp1 exp2) = compA exp2 ++ compA exp1 ++ [Add]
-compA (IntMult exp1 exp2) = compA exp2 ++ compA exp1 ++ [Mult]
-compA (IntSub exp1 exp2) = compA exp2 ++ compA exp1 ++ [Sub]
-
-compB :: Bexp -> Code
-compB (BoolLit (BoolValue n))
-  | n         = [Tru]  
-  | otherwise = [Fals] 
-compB (BoolLit (BoolVariable var)) = [Fetch var]
-compB (BoolNeg exp) = compB exp ++ [Neg]
-compB (BoolAnd exp1 exp2) = compB exp2 ++ compB exp1 ++ [And]
-compB (BoolEqual exp1 exp2) = compB exp2 ++ compB exp1 ++ [Equ]
-compB (IntEqual exp1 exp2) = compA exp2 ++ compA exp1 ++ [Equ]
-compB (IntLe exp1 exp2) = compA exp2 ++ compA exp1 ++ [Le]
-
-compile :: Program -> Code
-compile [] = []
-
-compile (statement : rest) =
-  case statement of
-    AssignIntStm var aExp -> compA aExp ++ [Store var] ++ compile rest
-    AssignBoolStm var bExp -> compB bExp ++ [Store var] ++ compile rest
-    --IfStm bexp ifBlock elseBlock -> compA aExp ++ [Store var] ++ compile rest
-    LoopStm bExp loopBody -> Loop (compB bExp) (compile loopBody) : compile rest
-
--- parse :: String -> Program
-parse = undefined -- TODO
-
--- To help you test your parser
---testParser :: String -> (String, String)
---testParser programCode = (stack2Str stack, store2Str store)
---  where (_,stack,store) = run(compile (parse programCode), createEmptyStack, createEmptyStore)
-
-
-
-data Token = PlusTok
-          | TimesTok
-          | OpenTok
-          | CloseTok
-          | SubTok
-          | IntEqualTok
-          | LessEqualTok
-          | BoolEqualTok
-          | AndTok
-          | NegTok
-          | AssignTok
-          | IfTok
-          | ThenTok
-          | ElseTok
-          | WhileTok
-          | DoTok
-          | EndTok
-          | IntTok Int
-          | BoolTok Bool
-          | VarTok String
-          deriving (Show)
-
-lexer :: String -> [Token]
-lexer [] = []
-
--- Symbols
-lexer (';' : rest) = EndTok : lexer rest
-lexer ('+' : rest) = PlusTok : lexer rest
-lexer ('-' : rest) = SubTok : lexer rest
-lexer ('*' : rest) = TimesTok : lexer rest
-lexer ('(' : rest) = OpenTok : lexer rest
-lexer (')' : rest) = CloseTok : lexer rest
-lexer (':' : '=' : rest) = AssignTok : lexer rest
-lexer ('<' : '=' : rest) = LessEqualTok : lexer rest
-lexer ('=' : '=' : rest) = IntEqualTok : lexer rest
-lexer ('=' : rest) = BoolEqualTok : lexer rest
-
--- If statements
-lexer ('i' : 'f' : rest) = IfTok : lexer rest
-lexer ('t' : 'h' : 'e': 'n' : rest) = ThenTok : lexer rest
-lexer ('e' : 'l' : 's': 'e' : rest) = ElseTok : lexer rest
-
--- Boolean operations
-lexer ('n' : 'o' : 't': rest) = NegTok : lexer rest
-lexer ('a' : 'n' : 'd': rest) = AndTok : lexer rest
-
--- While loops
-lexer ('w' : 'h' : 'i': 'l': 'e' : rest) = WhileTok : lexer rest
-lexer ('d' : 'o' : rest) = DoTok : lexer rest
-
--- Boolean values
-lexer ('T' : 'r' : 'u': 'e' : rest) = BoolTok True : lexer rest
-lexer ('F' : 'a' : 'l': 's': 'e' : rest) = BoolTok False : lexer rest
-
--- Space
-lexer (chr : rest)
-    | isSpace chr
-    = lexer rest
-
--- Integer values
-lexer str@(chr : _)
-    | isDigit chr
-    = IntTok (stringToInt integer) : lexer rest
-    where
-      (integer, rest) = span isDigit str
-      stringToInt :: String -> Int
-      stringToInt=foldl (\acc chr->10*acc+digitToInt chr) 0
-
--- Variables
-lexer str@(chr : _)
-    | isLower chr
-    = VarTok variable : lexer rest
-    where
-      (variable, rest) = span isVariable str
-      isVariable :: Char -> Bool
-      isVariable c = not (isSpace c) && isAlphaNum c && c `notElem` ['+', '-', '*', ':', '=', ';', '<', ')', '(']
-
--- Error handling
-lexer (chr : rest) = error ("unexpected character: ’" ++ show (chr) ++ "’")
-
--- Examples:
--- testParser "x := 5; x := x - 1;" == ("","x=4")
--- testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
--- testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)" == ("","x=1")
--- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
--- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
--- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
--- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
-
-testProgram :: Program
--- y := 1; while ¬(x = 1) do (y := y ∗ x; x := x − 1)
-testProgram = [AssignIntStm "y" (IntLit (IntValue 1)), LoopStm (BoolNeg (IntEqual (IntLit (IntVariable "x")) (IntLit (IntValue 1)))) [AssignIntStm "y" (IntMult (IntLit (IntVariable "y")) (IntLit (IntVariable "x"))), AssignIntStm "x" (IntSub (IntLit (IntVariable "x")) (IntLit (IntValue 1)))]]
-
---testProgram = [AssignBoolStm "x" (BoolNeg (BoolEqual (BoolVariable "x") (BoolValue True)))]
-main :: IO ()
-main = do
-    let compiledCode = compile testProgram
-    print compiledCode
-
 
