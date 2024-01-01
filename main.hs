c314231acbe74fa732e4cfc9e64e791cb9996bc2
LuisDu902 <luisdu.s23@gmail.com>
2024-01-01 23:30:31 +0000

    adding main function

--- main.hs
+++ main.hs
@@ -393,3 +393,64 @@ testParser :: String -> (String, String)
 testParser programCode = (stack2Str stack, state2Str state)
   where (_,stack,state) = run (compile (parse programCode), createEmptyStack, createEmptyState)
 
+main :: IO ()
+main = do
+  putStrLn "Running testAssembler tests:"
+  let a = testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
+  let b = testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
+  let c = testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
+  let d = testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
+  let e = testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
+  let f = testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
+  let g = testAssembler [Push (-20),Push (-21), Le] == ("True","")
+  let h = testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
+  let i = testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
+  -- If you test:
+  -- testAssembler [Push 1,Push 2,And]
+  -- You should get an exception with the string: "Run-time error"
+  -- If you test:
+  -- testAssembler [Tru,Tru,Store "y", Fetch "x",Tru]
+  -- You should get an exception with the string: "Run-time error"
+
+  let results = [ ("Test 1", a)
+                , ("Test 2", b)
+                , ("Test 3", c)
+                , ("Test 4", d)
+                , ("Test 5", e)
+                , ("Test 6", f)
+                , ("Test 7", g)
+                , ("Test 8", h)
+                , ("Test 9", i)
+                ]
+
+  mapM_ (\(testName, result) -> putStrLn $ testName ++ ": " ++ if result then "Passed" else "Failed") results
+  putStrLn "Running testParser tests:"
+  let a1 = testParser "x := 5; x := x - 1;" == ("","x=4")
+  let b1 = testParser "x := 0 - 2;" == ("","x=-2")
+  let c1 = testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
+  let d1 = testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("","x=1")
+  let e1 =  testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
+  let f1 =  testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
+  let g1 =  testParser "x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;" == ("","x=34,y=68")
+  let h1 =  testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("","x=34")
+  let i1 =  testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("","x=1")
+  let j1 =  testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
+  let k1 =  testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
+  let l1 =  testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+
+
+  let results2 = [ ("Test 1", a1)
+                , ("Test 2", b1)
+                , ("Test 3", c1)
+                , ("Test 4", d1)
+                , ("Test 5", e1)
+                , ("Test 6", f1)
+                , ("Test 7", g1)
+                , ("Test 8", h1)
+                , ("Test 9", i1)
+                , ("Test 10", j1)
+                , ("Test 11", k1)
+                , ("Test 12", l1)
+                ]
+
+  mapM_ (\(testName, result) -> putStrLn $ testName ++ ": " ++ if result then "Passed" else "Failed") results2
